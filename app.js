1f9ce60c7f7d9aa4d6129ff088279583c48b7766
Haruna Alvin <harunaalvin69@gmail.com>
2022-04-24 00:29:09 +0100

    added event listeners and functions for adding and deleting items

--- app.js
+++ app.js
@@ -2,5 +2,21 @@
 const expenseName = document.getElementById("expense-name")
 const expenseAmount = document.getElementById("expense-amount")
 const expenseDate = document.getElementById("expense-date")
+const saveExpense = document.getElementById("save-input")
 const expenseTable = document.getElementById("Expenses")
 const expenseRow = document.getElementById("expense-row")
+
+// Event Listeners
+saveExpense.addEventListener("click", addExpense)
+expenseTable.addEventListener("click", deleteButton)
+
+// -Functions
+
+// add expense function 
+function addExpense(){
+    
+    
+}
+
+//  dlete button function
+function deleteButton(){}
