0a31f40084f2a60ca229049f9532f5991d0c7d3c
Haruna Alvin <harunaalvin69@gmail.com>
2022-04-23 21:46:11 +0100

    created js file and grabbed html elements

---
+++ app.js
@@ -0,0 +1,6 @@
+// Grab Elements
+const expenseName = document.getElementById("expense-name")
+const expenseAmount = document.getElementById("expense-amount")
+const expenseDate = document.getElementById("expense-date")
+const expenseTable = document.getElementById("Expenses")
+const expenseRow = document.getElementById("expense-row")
