46101f563531d66c105237ea8a7cef7ff65d5382
Haruna Alvin <harunaalvin69@gmail.com>
2022-04-24 19:42:13 +0100

    assigned cells in table

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ function addExpenses(event){
         return alert("fill all fields");
     }
 
+    // assign cells to table
+    let i = 1
+    const row = expenses.insertRow(i)
+    const cell1 = row.insertCell(0)
+    const cell2 = row.insertCell(1)
+    const cell3 = row.insertCell(2)
+    const cell4 = row.insertCell(3)
     
 }
 
